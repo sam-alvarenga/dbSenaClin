c76f57c1524379cad4acda16f40d8296bea8692a
Samantha <samantha.c.alvarenga@gmail.com>
2024-06-05 01:56:23 -0300

    Exercicio 1

---
+++ Dml_Exercicios.sql
@@ -0,0 +1,17 @@
+/* Exercicios - Dml*/
+
+/*Exercicio 1.Adicionar à tabela Paciente um novo atributo, chamado Cidade; */
+ALTER TABLE paciente ADD COLUMN cidade VARCHAR(50) NOT NULL;
+
+SELECT * FROM paciente
+
+
+
+
+
+
+
+		
+
+		
+
